16667289a830992db2e0eb1c73a0dbc6596098de
Godwin Chijioke <cjdaveeds@gmail.com>
2025-06-02 17:29:04 +0100

    Add files via upload

---
+++ RandomNumberGenerator.py
@@ -0,0 +1,28 @@
+import random
+
+first_number = 0
+second_number
+counter = 0
+count = 0 
+total = 0
+
+for number in range(0 , 10):
+
+	first_number = random.randint(0 , 100)
+	second_number = random.randint(0 , 100)
+
+if maximum > minimum:
+
+	userinput = input( " What is ", first_number - second_number)
+ 
+	total = first_number - second_number
+
+	while (total <= 2):
+
+		if total == userinput:
+			count = count + 1
+print( "correct score is ",  count)
+
+	elif(total != userinput)
+			counter = counter + 1
+print(" failed score is " ,  counter)
